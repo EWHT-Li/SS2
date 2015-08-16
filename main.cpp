2aa718051b6c1459304130f82ba7dcfcb8f53e98
LittleBBB <1071247042@qq.com>
2015-08-16 16:14:57 +0800

    试试而已

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//#include "mainwindow.h"
+
+#include"easygame.h"
+#include <QApplication>
+#include <QTextCodec>
+#include <QTimer>
+
+int main(int argc,char **argv)
+{
+    QApplication app(argc,argv);
+    EasyGame w;
+    w.show();
+
+    return app.exec();
+}
+
